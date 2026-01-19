3f186da4ca2062efb90cd573d04a69f0ecb637e3
leny <lenygarcia0302@gmail.com>
2026-01-19 10:57:19 +0100

    Enhance NavbarProjects component with selection handling and styling

--- src/components/navbar_projects.tsx
+++ src/components/navbar_projects.tsx
@@ -1,16 +1,43 @@
 import '../styles/navbar_projects.css'
 import { useState } from 'react';
 
-function NavbarProjects() {
+interface NavbarProjectsProps {
+    onSelectionChange: (type: string) => void;
+}
+
+function NavbarProjects({ onSelectionChange }: NavbarProjectsProps) {
+    //state
     const [isAISelected, setIsAISelected] = useState(true);
     const [isWebSelected, setIsWebSelected] = useState(false);
+    const [style_ai, setStyleAI] = useState('elt_selected');
+    const [style_web, setStyleWeb] = useState('elt');
 
+    //handlers
+    const handleAISelection = () => {
+        if (isAISelected) return;
+        console.log("AI selected");
+        setIsAISelected(true);
+        setIsWebSelected(false);
+        setStyleAI('elt_selected');
+        setStyleWeb('elt');
+        onSelectionChange('AI');
+    };
+
+    const handleWebSelection = () => {
+        if (isWebSelected) return;
+        console.log("Web selected");
+        setIsAISelected(false);
+        setIsWebSelected(true);
+        setStyleAI('elt');
+        setStyleWeb('elt_selected');
+        onSelectionChange('Web');
+    };
 
     return (
         <nav className="navbar-projects">
-            <div className='elt'><p>AI</p></div>
+            <div className={style_ai} onClick={handleAISelection}><p>AI</p></div>
             <div className='column'></div>
-            <div className='elt'><p>Web</p></div>
+            <div className={style_web} onClick={handleWebSelection}><p>Web</p></div>
         </nav>
     );
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,15 +3,19 @@ import ai_projects from '../data/projects/ai_projects.json';
 import web_projects from '../data/projects/web_projects.json';
 import NavbarProjects from './navbar_projects';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 function Projects() {
     const { t } = useTranslation();
+    const [displayType, setDisplayType] = useState('AI');
+
+
     return (
         <div className="projects">
             <h1 className="section_title">{t("projects.title")}</h1>
             <p className="section_description">{t("projects.description")}</p>
             <div className="projects_section">
-                <NavbarProjects />
+                <NavbarProjects onSelectionChange={(type) => setDisplayType(type)}/>
             </div>
         </div>
     );
