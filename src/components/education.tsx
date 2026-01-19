74faf08c48d45e55dfb1613e18490f1b8af63eb4
leny <lenygarcia0302@gmail.com>
2026-01-19 14:30:47 +0100

    Add education and skills sections with dynamic data rendering and styling

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,5 +1,6 @@
 import '../styles/education.css';
 import { useTranslation } from 'react-i18next';
+import schools from '../data/education/schools.json';
 
 function Education() {
     const { t } = useTranslation();
@@ -7,6 +8,17 @@ function Education() {
         <div className="education">
             <h1 className="section_title">{t("education.title")}</h1>
             <p className="section_description">{t("education.description")}</p>
+            <div className="education_container">
+                {schools.map((school, index) => (
+                    <div key={index} className="education_card">
+                        <img src={school.logo} alt={school.name} className="education_logo" />
+                        <h2 className="education_name">{school.name}</h2>
+                        <h3 className="education_degree">{school.degree}</h3>
+                        <p className="education_period">{school.startDate} --- {school.endDate}</p>
+                        <p className="education_description">{school.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,5 +1,6 @@
 import '../styles/skills.css';
 import { useTranslation } from 'react-i18next';
+import skills from '../data/skills/skills.json';
 
 function Skills() {
     const { t } = useTranslation();
@@ -7,6 +8,14 @@ function Skills() {
         <div className="skills">
             <h1 className="section_title">{t("skills.title")}</h1>
             <p className="section_description">{t("skills.description")}</p>
+            <div className="skills_container">
+                {skills.map((skill, index) => (
+                    <div key={index} className="skill_card">
+                        <h2 className="skill_name">{skill.name}</h2>
+                        <p className="skill_description">{skill.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
